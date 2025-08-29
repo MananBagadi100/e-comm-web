58181f997639e68d381ae479624ff2d30a654505
MananBagadi100 <mananbagadi100@gmail.com>
2025-08-29 18:06:05 +0530

    context for cart functionality

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,18 @@
 import UserDetails from "../components/UserDetails"
 import { Outlet } from "react-router-dom"
+import { useContext } from "react"
+import { counterContext } from "../context/context_pro"
 const About= () => {
-
+    const value=useContext(counterContext)
     return (
         <div>
             <div>call user id 500</div>
             <div>call user id 600</div>
             <div>call user id 700</div>
             <div><Outlet /></div>
+            <button onClick={()=> (value.setCounter(value.counter+1))}>
+                Hey ({value.counter})
+            </button>
         </div>
     )
 }
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react"
+import { counterContext } from "../context/context_pro"
 const ContactUs= () => {
+    const value=useContext(counterContext)
     return (
-        <div>This is Contact us page</div>
+        <div>
+            <div>This is Contact us page</div>
+            <button onClick={()=> value.setCounter(value.counter+1)}>
+                Hey ({value.counter})
+            </button>
+        </div>
+        
     )
 }
 export default ContactUs
