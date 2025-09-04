470ae10664572a6f10955f9191198bb597dad5c3
MananBagadi100 <mananbagadi100@gmail.com>
2025-09-04 16:39:47 +0530

    Added login context and some minor details

---
+++ src/context/LoginContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+
+export const LoginContext = createContext()
+export const LoginProvider = (props) => {
+    const [ loginState , setLoginState] = useState(false)
+    return (
+        <LoginContext.Provider value={{loginState,setLoginState}}>
+            {props.children}
+        </LoginContext.Provider>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import { ContextProvider } from './context/context_pro.jsx'
 import { CartProvider } from './context/CartContext.jsx'
+import { LoginProvider } from './context/LoginContext.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <LoginProvider>
     <CartProvider>
     <ContextProvider>
       <App />
     </ContextProvider>
     </CartProvider>
+    </LoginProvider>
   </StrictMode>,
 )
