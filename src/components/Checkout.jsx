359c77bc11fd822c2a75a71d750f82632cd2daff
MananBagadi100 <mananbagadi100@gmail.com>
2025-09-07 12:39:59 +0530

    Checkout UI done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,37 @@
+import { useContext } from "react"
+import { cartContext } from "../context/CartContext"
+import '../styles/CheckoutStyles.css'
+
 const Checkout = () => {
+    const cartValue = useContext(cartContext)
+    const calculateSubTotal = (prod) => {
+        return ((prod.price * prod.quantity).toFixed(2))
+    }
     return (
-        <div>
-            <div>Hey this is checkout page</div>
+        <div className="full-page-container">
+            <div className="all-info-container">
+                <div>Hey this is checkout page</div>
+                <div>Order details</div>
+                <div className="item-list">
+                    {
+                        cartValue.cart.map((prod) => (
+                            <div className="eachProductCard" key={prod.id}>
+                                <img id="prod-image" src={prod.image} alt="Image not found" />
+                                <div id="prod-details">
+                                    <div id="prod-title">{prod.title}</div>
+                                    <div id="prod-price">Price: {prod.price}</div>
+                                <div id="prod-quantity">Quantity : {prod.quantity}</div>
+                                </div>
+                                <div id="prod-subTotal-wrapper">
+                                    <div id="prod-subTotal">SubTotal : {calculateSubTotal(prod)}</div>
+                                </div>
+                            </div>
+
+
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     )
 }
