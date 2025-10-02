2836ba207bc29d28280b244f8fbac29d30455d17
MananBagadi100 <mananbagadi100@gmail.com>
2025-10-02 20:01:38 +0530

    Changed login UI little bit

--- my-react-app/src/pages/Login.jsx
+++ my-react-app/src/pages/Login.jsx
@@ -2,11 +2,23 @@ import '../styles/LoginStyle.css'
 import { useContext, useState } from 'react'
 import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import { LoginContext } from '../context/LoginContext'
-
+import { useForm } from 'react-hook-form'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import KeyIcon from '@mui/icons-material/Key';
+//storeLoginState is a global function to change the login state 
 const Login= () => {
+    const [userName,setUserName] =useState('')
+    const [password,setPassword] =useState('')
     const value = useContext(LoginContext)
     const location = useLocation()
     const navigate = useNavigate()
+    const {
+        register,
+        handleSubmit,
+        watch,
+        setError,   //for custom errors from server
+        formState: { errors,isSubmitting,},
+    } = useForm()
     const handleNameChange =(e) => {
         setUserName(e.target.value)
     }
@@ -14,45 +26,71 @@ const Login= () => {
         setPassword(e.target.value)
     }
     const handleSubmitBtnClick= () => {
-        if(!userName.trim() || !password.trim()){
-            alert("Please enter both your username and password")
-            return
-        }
-        else {      //user has logged in
-            value.storeLoginState(true)
-            const urlParams = new URLSearchParams(location.search)
-            console.log(urlParams)
-            const redirectTo = urlParams.get('redirect') || '/'
-            navigate(redirectTo,{replace: true})
-
-        }
+        // if(!userName.trim() || !password.trim()){
+        //     alert("Please enter both your username and password")
+        //     return
+        // }
+        // else {      //user has logged in
+        //     value.storeLoginState(true)
+        //     const urlParams = new URLSearchParams(location.search)
+        //     console.log(urlParams)
+        //     const redirectTo = urlParams.get('redirect') || '/'
+        //     navigate(redirectTo,{replace: true})
+        value.storeLoginState(true)
+        const urlParams = new URLSearchParams(location.search)
+        console.log(urlParams)
+        // const redirectTo = urlParams.get('redirect') || '/'
+        // navigate(redirectTo,{replace: true})
     }
     
-    const [userName,setUserName] =useState('')
-    const [password,setPassword] =useState('')
+    const onSubmit = async(data) => {
+        console.log('The name is ',data)
+    }
     if(value.loginState === false ) {
         return (
             <div className='login-container-wrapper'>
                 <div className='login-container'>
-                    <div className='heading-container'>Login</div>
-                    <div className='details-container'>
-                        <div className='name-container'>
-                            <label>Name : </label>
-                            <input type='text' 
-                            value={userName} onChange={handleNameChange}
-                            placeholder='Enter your name'>
-                            </input>
+                    <div className='login-heading-container'>Login</div>
+                    <form className='login-details-container' onSubmit={handleSubmit(onSubmit)}>
+                        <div className='login-input-container-wrapper'>
+                            <div className="login-input-container">
+                                <div className='login-field-input-icons'>
+                                    <AccountCircleIcon sx={{fontSize:36}}/>
+                                </div>
+                                <input type='text'
+                                    className='login-input-fields' 
+                                    // value={userName} 
+                                    // onChange={handleNameChange}
+                                    {...register("username",
+                                        {required:true,minLength:{value:5,message:"Min length should be 5"},
+                                        maxLength:{value:35,message:"Max length should not exceed 35"}})}
+                                    placeholder='Enter your name'>
+                                </input>
+                            </div>
+                            {errors.username && <div>{errors.username.message}</div>}
                         </div>
-                        <div className='password-container'>
-                            <label>Password : </label>
-                            <input type='password'
-                            value={password} onChange={handlePasswordChange}
-                            placeholder='Enter your password'>
-                            </input>
+                        <div className='login-input-container-wrapper'>
+                            <div className="login-input-container">
+                                <div className='login-field-input-icons'>
+                                    <KeyIcon sx={{fontSize:36}}/>
+                                </div>
+                                <input type='password'
+                                    className='login-input-fields' 
+                                    // value={userName} 
+                                    // onChange={handleNameChange}
+                                    {...register("password",
+                                        {required:true,minLength:{value:5,message:"Password min length should be 5"},
+                                        maxLength:{value:35,message:"Password max length should not exceed 35"}})}
+                                    placeholder='Enter your password'>
+                                </input>
+                            </div>
+                            {errors.username && <div>{errors.username.message}</div>}
                         </div>
-                        <button className='submit-btn' type='submit' onClick={handleSubmitBtnClick}>Submit</button>
+                        <div className="login-submit-btn-wrapper">
+                            <button className='submit-btn' type='submit'>Submit</button>
+                        </div>
                         <p className='registration-link'>New User? <a href='/register'>Register Now !</a></p>
-                    </div>
+                    </form>
                     
                 </div>
             </div>
